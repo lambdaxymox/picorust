62cf87be8307280c243c53446a8c22aaa47492d8
Stallmanifold <stallmanifold@gmail.com>
2016-06-12 01:36:29 -0500

    Add more ffi functions from the picosat api.

--- src/picosat.rs
+++ src/picosat.rs
@@ -1,6 +1,9 @@
-use libc::{c_char, c_int, c_void, c_double, size_t};
-use std::ffi::CStr;
+use libc::{c_char, c_int, c_void, c_double, size_t, c_uint};
+use libc;
+use std::ffi::{CStr, CString};
 use std::str;
+use std::fs::File;
+use std::os::unix::io::AsRawFd;
 
 
 const PICOSAT_VERSION: isize = 965;
@@ -20,6 +23,19 @@ extern {
 
     fn picosat_reset(picosat: *mut C_PicoSAT) -> c_void;
 
+    fn picosat_set_output(picosat: *mut C_PicoSAT, file: *mut libc::FILE) -> c_void;
+
+    fn picosat_measure_all_calls(picosat: *const C_PicoSAT) -> c_void;
+
+    fn picosat_set_prefix(picosat: *mut C_PicoSAT, prefix: *const c_char) -> c_void;
+    fn picosat_set_verbosity(picosat: *mut C_PicoSAT, new_verbosity_level: c_int) -> c_void;
+    fn picosat_set_plain(picosat: *mut C_PicoSAT, new_plain_value: c_int) -> c_void;
+    fn picosat_set_global_default_phase(picosat: *mut C_PicoSAT, phase: c_int) -> c_void;
+    fn picosat_set_default_phase_lit(picosat: *mut C_PicoSAT, lit: c_int, phase: c_int) -> c_void;
+    fn picosat_reset_phases(picosat: *mut C_PicoSAT) -> c_void;
+    fn picosat_reset_scores(picosat: *mut C_PicoSAT) -> c_void;
+    fn picosat_remove_learned(picosat: *mut C_PicoSAT, percentage: c_uint) -> c_void;
+
     fn picosat_inc_max_var(picosat: *mut C_PicoSAT) -> c_int;
 
     fn picosat_push(picosat: *mut C_PicoSAT) -> c_int;
@@ -36,7 +52,7 @@ extern {
     fn picosat_time_stamp() -> c_double;
     fn picosat_stats(picosat: *const C_PicoSAT) -> c_void;
     fn picosat_seconds(picosat: *const C_PicoSAT) -> c_double;
-}
+} // end C function FFI declarations.
 
 pub fn copyright() -> &'static str {
     let c_buf: *const c_char = unsafe { picosat_copyright() };
@@ -83,6 +99,68 @@ pub fn init() -> PicoSAT {
 pub fn reset(picosat: &mut PicoSAT) {
     unsafe {
         picosat_reset(&mut *picosat.ptr);
+    }
+}
+
+pub fn set_output(picosat: &mut PicoSAT, file: &mut File) {
+    unsafe {
+        let fd = file.as_raw_fd();
+        let c_mode = CString::new("rw").unwrap();
+        let c_mode_ptr = c_mode.as_ptr();
+        let c_file = libc::fdopen(fd, c_mode_ptr);
+
+        picosat_set_output(&mut *picosat.ptr, c_file);
+    }
+}
+
+pub fn measure_all_calls(picosat: &PicoSAT) {
+    unsafe {
+        picosat_measure_all_calls(&*picosat.ptr);
+    }
+}
+
+pub fn set_prefix(picosat: &mut PicoSAT, prefix: &[u8]) {
+    unsafe {
+        let c_prefix = CStr::from_bytes_with_nul_unchecked(prefix);
+        let c_ptr    = c_prefix.as_ptr();
+
+        picosat_set_prefix(&mut *picosat.ptr, c_ptr);
+    }
+}
+
+pub fn set_verbosity(picosat: &mut PicoSAT, new_verbosity_level: i32) {
+    unsafe {
+        picosat_set_verbosity(&mut *picosat.ptr, new_verbosity_level);
+    }
+}
+
+pub fn set_plain(picosat: &mut PicoSAT, new_plain_value: i32) {
+    unsafe {
+        picosat_set_plain(&mut *picosat.ptr, new_plain_value);
+    }
+}
+
+pub fn set_global_default_phase(picosat: &mut PicoSAT, phase: i32) {
+    unsafe {
+        picosat_set_global_default_phase(&mut *picosat.ptr, phase);
+    }
+}
+
+pub fn set_default_phase_lit(picosat: &mut PicoSAT, lit: i32, phase: i32) {
+    unsafe {
+        picosat_set_default_phase_lit(&mut *picosat.ptr, lit, phase);
+    }
+}
+
+pub fn reset_phases(picosat: &mut PicoSAT) {
+    unsafe {
+        picosat_reset_phases(&mut *picosat.ptr);
+    }
+}
+
+pub fn remove_learned(picosat: &mut PicoSAT, percentage: u32) {
+    unsafe {
+        picosat_remove_learned(&mut *picosat.ptr, percentage);
     }
 }
 
